ba60a7854ad3a45198639b4f811d3e3874970902
Android250 <android250@zamt.local>
2024-12-16 15:58:28 +0300

    Добавили в проект заставку

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.smartlab.ui.theme.SmartlabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
