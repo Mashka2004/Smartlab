ad410c4e90c77271d1c4ab462a2b50c566e80c80
Android250 <android250@zamt.local>
2024-12-16 16:26:44 +0300

    Добавили файл TextButton и добавили логику

---
+++ app/src/main/java/com/example/smartlab/components/TextButton.kt
@@ -0,0 +1,22 @@
+package com.example.smartlab.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextButton(modifier: Modifier = Modifier,text: String,onClick: () -> Unit = {}) {
+    Text(
+        text = text,
+        modifier = modifier
+            .clickable { onClick() },
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+    )
+}
