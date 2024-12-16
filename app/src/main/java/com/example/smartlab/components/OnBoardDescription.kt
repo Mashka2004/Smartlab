9e30fb1857374826891a9e0e61300dc927db2866
Android250 <android250@zamt.local>
2024-12-16 16:07:01 +0300

    Добавили в проект необходимые цвета для текста

---
+++ app/src/main/java/com/example/smartlab/components/OnBoardDescription.kt
@@ -0,0 +1,21 @@
+package com.example.smartlab.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OnBoardDescription(modifier: Modifier = Modifier, text: String) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color =
+    )
+}
+
+@Preview
+@Composable
+private fun OnBoardDescriptionPreview() {
+
+}
--- app/src/main/java/com/example/smartlab/ui/theme/Color.kt
+++ app/src/main/java/com/example/smartlab/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.smartlab.ui.theme
 
+import android.content.res.Resources.Theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val ThemColor = Color(0xFF83A0F8)
+val TextButtonColor = Color(0xFF57A9FF)
+val OnBoardHeaderColor = Color(0xFF00B712)
+val OnBoardDescription = Color(0xFF939396)
